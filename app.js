a89f88d80108d5df97d7e5cb4d947aaaeb9ec82a
Keissy <keissy@lifecellskincare.com>
2018-01-18 10:25:38 -0500

    flashcards file created

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var cloze = require('.ClozeCard.js');
 var questions = require('.questions.js'); 
 
 
+var closeQuestions = []; 
 // Populate the cloze-deleted questions list 
 
 // need to review if this is right, ClozeCard.ClozeCard 
 
 for (var i = 0; i < questions.length; i++){ 
-	var q = new ClozeCard.ClozeCard(questions[i].full, questions[i].clone); 
+	var q = new ClozeCard(questions[i].full, questions[i].clone); 
 	closeQuestions.push(q); 
 
 }
@@ -41,21 +42,24 @@ function askQuestion(){
 		]).then(function (answers){ 
 			console.log('\n'); 
 
-			// check if the user has guessed correctly 
-			if (answes.userGuess.toLowerCase()=== closeQuestions [currentQuestion].cloze.toLowerCase()){ 
+// check if the user has guessed correctly 
+			if (answers.userGuess.toLowerCase()=== closeQuestions [currentQuestion].cloze.toLowerCase()){ 
 				console.log('You so smart!!'); 
 				answerRight++; 
+// adds to the wrong answer score 
 			} else { 
 				console.log('You pretty dumb!')
 				answerWrong++; 
 			}
+
+// gives answer 
 			console.log(closeQuestions[currentQuestion].full); 
 			console.log('-------------------------------------\n'); 
-
+// if hasnt lost, continues game 
 			if (currentQuestion < closeQuestions.length - 1) { 
 				currentQuestion++; 
 				askQuestion(); 
-
+// if game over, logs the game over message 
 			} else { 
 				console.log('GAME OVER LOSER!'); 
 				console.log('Correct Answers:' answerRight); 
@@ -72,6 +76,7 @@ function askQuestion(){
 						name: playAgain
 					}
 
+// if says yes to continuing game, this resets the score board. 
 					]).then(function(answer){ 
 					 if(answer.playAgain){ 
 					 	currentQuestion = 0; 
